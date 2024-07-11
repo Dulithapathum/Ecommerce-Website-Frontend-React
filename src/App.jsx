cfc0902d8d9aaae6bf514ddcab5718792e390cfa
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-07-11 09:58:38 +0530

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { About } from "./Pages/About";
+import { Shop } from "./Pages/Shop";
+import { Contact } from "./Pages/Contact";
+import { LogIn } from "./Components/LogIn";
+import { SignUp } from "./Components/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
